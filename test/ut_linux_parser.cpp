c9b94af91820792cb6ba4c830ef4ed76527ae480
lotape6 <lotape6@gmail.com>
2022-09-06 18:58:18 +0200

    Propperly configured tests

--- test/ut_linux_parser.cpp
+++ test/ut_linux_parser.cpp
@@ -6,12 +6,30 @@
 #include <optional>
 #include <vector>
 
+const std::string dummyPrefix{"/home/lotape6/Projects/NanodegreeC++/CppND-System-Monitor/test/dummy_files"};
+
 class utLinuxParser : public ::testing::Test
 {
-  private:
+
+  protected:
     FilesDictionary test_from;
 };
 
 TEST_F(utLinuxParser, testReadNewEntry)
 {
+    const std::string dummyProc{dummyPrefix + kProcDirectory + kStatFilename};
+    int user, nice, system, irq, softirq, steal, foo;
+    std::string cpu_name;
+
+    long cpu_usage0, cpu_usage1;
+
+    test_from[dummyProc].ReadLine(cpu_name, user, nice, system, foo, foo, irq, softirq, steal).update();
+
+    cpu_usage0 = user + nice + system + irq + softirq + steal;
+
+    test_from[dummyProc].ReadLine(cpu_name, user, nice, system, foo, foo, irq, softirq, steal).update();
+
+    cpu_usage1 = user + nice + system + irq + softirq + steal;
+
+    EXPECT_EQ(cpu_usage0, cpu_usage1);
 }
